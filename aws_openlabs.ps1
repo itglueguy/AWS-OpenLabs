d569a6029d74a342a1a26b25d675f9bf8de58668
itglueguy <38425660+itglueguy@users.noreply.github.com>
2018-09-05 21:04:16 -0700

    v1
    Initial version until all the functions are built

---
+++ aws_openlabs.ps1
@@ -0,0 +1,48 @@
+﻿<#
+Title:
+AWS OpenLabs v2.
+
+Last Updated:
+9/5/2018
+
+
+
+
+
+#>
+
+# Import the Powershell Module
+Import-Module AWSPowershell
+
+# dot source the powershell scripts.
+. "C:\Users\$env:username\Documents\GitHub\AWS-OpenLabs\aws_user_management.ps1"
+
+
+# set the credentials and Region
+$accessKey = "Key"
+$secretkey = "SecretKey"
+
+$Region = "us-east-1" 
+
+# Store the AWS Credentials
+Set-AWSCredential -StoreAs "LabProfile" -AccessKey $accesskey -SecretKey $secretkey
+
+# Set the AWS Region
+Set-DefaultAWSRegion -Region $Region
+
+# Get the Account ID of the User
+$accountID = (Get-STSCallerID).account
+$url = -join('https://',$accountID,'/signin.aws.amazon.com/console')
+
+
+#----------------------------------------
+
+# Get the List of Users
+$users = Get-content "C:\Users\$env:username\Documents\GitHub\AWS-OpenLabs\users.txt"
+
+# Specify the group you would like the users to be joined to ( Group = Lab Scenario )
+
+$group = "Intended Group"
+
+# Provision the New Users
+New-AWSIAMLabUsers -users $users -group
---
+++ aws_user_management.ps1
@@ -0,0 +1,64 @@
+﻿Function New-AWSIAMLabUsers ($users,$group) {
+    
+    # Loops through the list of users
+    $awsusers_hash = foreach($user in $users) {
+        
+        $aws_userhash = @{}
+
+        # New IAM User
+        $aws_userhash.username = $user 
+        $aws_userhash.createDate = (New-Iamuser -username $user).CreateDate
+
+        # use random characters to generate the password
+        $Password = ([char[]]([char]33..[char]95) + ([char[]]([char]97..[char]126)) + 0..9 | sort {Get-Random})[0..8] -join ''
+
+        # Set the password to the profile in question
+        New-IAMLoginProfile -username $user -Password $Password
+        
+        # Create a new Password for the user
+        $aws_userhash.password = $Password
+
+        # Add the user to a particular Group
+        Add-IAMUserToGroup -UserName $user -GroupName $group
+
+        $aws_userhash.group = $group
+
+        # Specify the login url for use later
+        $aws_userhash.url = $url
+
+        # output the object
+        $aws_userhash
+    }
+
+    # output the object to the pipeline
+    $aws_usershash
+
+}
+
+Function Remove-AWSIAMLabUsers ($users,$group) {
+    
+    # Loops through the list of users
+    foreach($user in $users) {
+        
+        $aws_userhash = @{}
+
+        # use random characters to generate the password
+        $Password = ([char[]]([char]33..[char]95) + ([char[]]([char]97..[char]126)) + 0..9 | sort {Get-Random})[0..8] -join ''
+
+        # Update the profile password in question
+        New-IAMLoginProfile -username $user -Password $Password
+        
+        # Remove the user from a particular Group
+        Remove-IAMUserfromGroup -UserName $user -GroupName $group
+
+        # Delete the IAM User
+
+    }
+
+
+}
+
+
+
+
+
